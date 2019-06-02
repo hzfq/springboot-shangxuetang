0728eb3337093d28489671101a1839489509bff7
huzhifengqing <1443754157@qq.com>
2019-06-02 20:47:49 +0800

    SimpleMappingExceptionResolver处理异常

--- 17-spring-boot-exception4/src/main/java/hzfq/exception/exception/GlobalException.java
+++ 17-spring-boot-exception4/src/main/java/hzfq/exception/exception/GlobalException.java
@@ -1,9 +1,13 @@
 package hzfq.exception.exception;
 
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 /**
- * 全局异常处理
+ * 利用{@code SimpleMappingExceptionResolver}全局异常处理,无法传递错误异常信息
  * 
  * @author huzhifengqing
  * @email huzhifengqing@qq.com
@@ -12,4 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GlobalException {
 
+    /**
+     * 该方法返回值必须是{@code SimpleMappingExceptionResolver}
+     * 
+     * @return
+     */
+    @Bean
+    public SimpleMappingExceptionResolver simpleMappingExceptionResolver() {
+        SimpleMappingExceptionResolver resolver = new SimpleMappingExceptionResolver();
+
+        // key为异常全类名,value为错误视图名
+        Properties mappings = new Properties();
+        mappings.setProperty("java.lang.ArithmeticException", "error1");
+        mappings.setProperty("java.lang.NullPointerException", "error2");
+
+        resolver.setExceptionMappings(mappings); // 映射异常类名和错误视图名
+        return resolver;
+    }
 }
